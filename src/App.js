868c2f3dc5e3e20e32a97d7addeed1dea8ea4671
Juan <juanignacioponciano0@gmail.com>
2023-04-05 15:51:00 -0300

    commmitt

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <form onSubmit={contarvocales}>
+        <p>Ingrese texto:
+          <input type="text" name="texto"  id='texto'/>
+        </p>
+        <p>
+          <input type="submit" value="Alerta" />
+        </p>
+      </form>
+    </div>
+  );
+}
+
+
+
+function contarvocales(c){
+  c.preventDefault();
+  let a1=String(c.target.texto.value);
+  const ver=a1;
+ let contadorvocales = 0;
+ const vocales = "aeiouAEIOU";
+
+ for(let i = 0; i < c.target.texto.value.length; i++){
+  if (vocales.indexOf(c.target.texto.value[i]) !==-1 ){
+    contadorvocales++;
+  }
+} 
+
+alert('Dice: '+ver+' Tiene: '+contadorvocales+' vocales')
+
+
+
+}
+
+export default App;
